68b8e91165173c17370ed352228ecb56c3d1cd8a
kleimaj <jacobakleiman@gmail.com>
2021-01-18 13:12:52 -0800

    Adds movement area padding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
 
     float xMin;
     float xMax;
@@ -17,13 +18,13 @@ public class Player : MonoBehaviour {
     private void SetUpMoveBoundaries() {
         Camera gameCamera = Camera.main;
         // get minimum x world space value
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
         // get maximum x world space 
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
         // get minimum y world space value
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
         // get maximum y world space 
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
 
     }
 
