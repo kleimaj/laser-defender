f7e03daf0eb3e5e04e72bc550b9f2db7a8dbe045
kleimaj <jacobakleiman@gmail.com>
2021-01-18 22:52:51 -0800

    Adds OnTrigger projectile collision functionality to Enemy

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,10 +8,14 @@ public class EnemySpawner : MonoBehaviour {
     // config vars
     [SerializeField] List<WaveConfig> waveConfigs;
     [SerializeField] int startingWave = 0;
+    [SerializeField] bool looping = false;
 
     // Start is called before the first frame update
-    void Start() {
-        StartCoroutine(SpawnAllWaves());
+    IEnumerator Start() {
+        do {
+            yield return StartCoroutine(SpawnAllWaves());
+        }
+        while (looping);
     }
     
     private IEnumerator SpawnAllWaves() {
